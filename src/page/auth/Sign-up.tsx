3202e9623124d0a2fbc1c2d4498f6ffb89a38cbb
Owais rao <owaisrao405@gmail.com>
2025-12-29 17:46:26 +0500

    Google Remove

--- src/page/auth/Sign-up.tsx
+++ src/page/auth/Sign-up.tsx
@@ -92,14 +92,6 @@ const SignUp = () => {
               <Form {...form}>
                 <form onSubmit={form.handleSubmit(onSubmit)}>
                   <div className="grid gap-6">
-                    <div className="flex flex-col gap-4">
-                      <GoogleOauthButton label="Signup" />
-                    </div>
-                    <div className="relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t after:border-border">
-                      <span className="relative z-10 bg-background px-2 text-muted-foreground">
-                        Or continue with
-                      </span>
-                    </div>
                     <div className="grid gap-2">
                       <div className="grid gap-2">
                         <FormField
